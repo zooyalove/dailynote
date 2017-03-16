13ed918922b0251f90d842e250e9d1bda0df82a9
zooyalove <dorry457@nate.com>
2017-03-17 00:36:33 +0900

    서버 시작방법 변경

--- daily-frontend/scripts/start.js
+++ daily-frontend/scripts/start.js
@@ -257,11 +257,11 @@ function runDevServer(host, port, protocol) {
     },
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === "https",
-    // host: host
-    host: '0.0.0.0',
-    proxy: {
-      "*": "http://localhost:4000"
-    }
+    host: host
+    //host: '0.0.0.0',
+    // proxy: {
+    //   "*": "http://localhost:4000"
+    // }
   });
 
   // Our custom middleware proxies requests to /index.html or a remote API.
