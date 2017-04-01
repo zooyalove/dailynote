8892045fbdd35403c1ff59844c7ade0017b6407e
zooyalove <dorry457@nate.com>
2017-04-01 19:28:03 +0900

    Login form input modified

--- daily-frontend/src/containers/routes/LoginRoute.js
+++ daily-frontend/src/containers/routes/LoginRoute.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Checkbox, Icon, Input } from 'semantic-ui-react';
+import axios from 'axios';
 
 import * as header from './../../redux/modules/base/header';
 import * as login from './../../redux/modules/base/login';
@@ -56,13 +57,20 @@ class LoginRoute extends Component {
         LoginActions.setLoginFormInfo({name, value});
     }
 
+    handleRemember = (e, data) => {
+        const { LoginActions } = this.props;
+        LoginActions.setLoginFormRemember(!data.checked);
+    }
+
     handleSubmit = (e) => {
         const { LoginActions } = this.props;
         LoginActions.clearLoginFormInfo();
     }
 
     render() {
-        const { handleChange, handleSubmit } = this;
+        const { handleChange, handleRemember, handleSubmit } = this;
+        const { status: { login }} = this.props;
+        const checked = login.get('is_remember');
 
         return (
             <div
@@ -91,7 +99,7 @@ class LoginRoute extends Component {
                             onTextChange={handleChange}
                         />
                         <div className="login-remember">
-                            <Checkbox label="Remember me" defaultChecked />
+                            <Checkbox label="Remember me" checked={checked} onClick={handleRemember} />
                         </div>
                         <div className="login-item">
                             <button className="login-submit" tabIndex="3" onClick={handleSubmit} >LOGIN</button>
--- daily-frontend/src/redux/modules/base/login.js
+++ daily-frontend/src/redux/modules/base/login.js
@@ -3,24 +3,34 @@ import { Map } from 'immutable';
 
 /* actions */
 const LOGIN_FORM_INFO_SET = "login/LOGIN_FORM_INFO_SET";
+const LOGIN_FORM_REMEMBER_SET = "login/LOGIN_FORM_REMEMBER_SET";
 const LOGIN_FORM_INFO_CLEAR = "login/LOGIN_FORM_INFO_CLEAR";
 
 /* action creators */
 export const setLoginFormInfo = createAction(LOGIN_FORM_INFO_SET);
+export const setLoginFormRemember = createAction(LOGIN_FORM_REMEMBER_SET);
 export const clearLoginFormInfo = createAction(LOGIN_FORM_INFO_CLEAR);
 
 const initialState = Map({
-    userid: null,
-    password: null
+	loginForm: Map({
+	    userid: null,
+	    password: null
+	}),
+	is_remember: true
 });
 
 export default handleActions({
 	[LOGIN_FORM_INFO_SET]: (state, action) => {
-        const {name, value} = action.payload;
+        const { name, value } = action.payload;
 
-		return state.set(name, value);
+		return state.setIn(['loginForm', name], value);
+    },
+    [LOGIN_FORM_REMEMBER_SET]: (state, action) => {
+    	const checked = action.payload;
+
+    	return state.set('is_remember', checked);
     },
 	[LOGIN_FORM_INFO_CLEAR]: (state, action) => (
-		state.merge(initialState)
+		state.mergeIn('loginForm', ...initialState.get('loginForm'))
 	)
 }, initialState);
