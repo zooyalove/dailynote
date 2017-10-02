70176a5fea85ab1a913579f30f44b937b068139a
Park Kyungbong <dorry457@nate.com>
2017-10-02 18:57:58 +0900

    거래처 총 갯수 추가

--- daily-frontend/src/components/Orderer/OrdererList.js
+++ daily-frontend/src/components/Orderer/OrdererList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const OrdererList = ({children}) => {
+const OrdererList = ({children, count}) => {
 	return (
 		<div className="orderer-list">
-			<div className="orderer-title">거래처 리스트</div>
+			<div className="orderer-title">거래처 리스트 ({count})</div>
 			<div className="orderer-list-item">
 				{children}
 			</div>
