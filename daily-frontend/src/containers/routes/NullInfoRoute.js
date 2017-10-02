565ce92d4b3d9593b59a01993ebcfd13f1f6a4e3
Park Kyungbong <dorry457@nate.com>
2017-10-02 18:56:22 +0900

    거래처 메인 인덱스 페이지 추가

---
+++ daily-frontend/src/containers/routes/NullInfoRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { OrdererInfo } from 'components/Orderer';
+
+const NullInfoRoute = () => {
+    return (
+        <OrdererInfo noid>거래처 정보 메인페이지입니다...</OrdererInfo>
+    );
+}
+
+export default NullInfoRoute;
