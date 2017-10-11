c2b45f8935eed1e1ab355c5729f9264507faab11
Park Kyungbong <dorry457@nate.com>
2017-10-11 19:07:48 +0900

    거래처 통계페이지 잠시 수정

--- daily-frontend/src/containers/routes/NullInfoRoute.js
+++ daily-frontend/src/containers/routes/NullInfoRoute.js
@@ -6,13 +6,7 @@ import ChartCard from 'components/ChartCard';
 class NullInfoRoute extends Component {
     render() {
         return (
-            <OrdererInfo noid>거래처 정보 메인페이지입니다...
-                <Card title="Statistics">
-                    지금은 테스트중입니다...
-                </Card>
-                <Card title="Statistics" transparent>
-                    지금은 테스트중입니다...
-                </Card>
+            <OrdererInfo noid>
                 <ChartCard/>
             </OrdererInfo>
         );
