64b4217b929a4858faa839f5b704b65284ae7981
Park Kyungbong <dorry457@nate.com>
2017-12-06 21:11:22 +0900

    일시적 메인화면 변경

--- daily-frontend/src/containers/routes/MainRoute.js
+++ daily-frontend/src/containers/routes/MainRoute.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import moment from 'moment';
 
 import * as header from 'redux/modules/base/header';
 
 import storage from 'helpers/storage';
+import * as api from 'helpers/WebApi/note';
 
 class MainRoute extends Component {
     static contextTypes = {
@@ -23,10 +25,18 @@ class MainRoute extends Component {
 		}
 	}
 
+	getTodayData = async () => {
+		const result = await api.getTodayNotes();
+		console.log(result);
+	}
+
 	render() {
+		const { getTodayData } = this;
+		getTodayData().catch( (err) => console.log(err) );
+
 		return (
 			<div className="subcontents-wrapper">
-				<h3>오늘의 주문내역</h3>
+				<h3>오늘 ({moment().format("YYYY-MM-DD")})의 주문내역</h3>
 			</div>
 		);
 	}
