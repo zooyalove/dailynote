131d618e9852f9cae618189f14ebd92a31bab93a
ZooyaLove <dorry457@nate.com>
2017-12-12 03:20:44 +0900

    검색입력폼 컴포넌트 추가가

---
+++ daily-frontend/src/components/SearchInput/SearchInput.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from './SearchInput.scss';
+import Input from 'components/Input';
+
+const cx = classNames.bind(styles);
+
+let value = "";
+
+const SearchInput = ({
+    fullWidth,
+    icon,
+    onSearch,
+    ...rest
+}) => {
+    return (
+        <div className={cx('search-input', {
+            'full-width': fullWidth,
+            icon
+        })}>
+            <Input
+                fullWidth={fullWidth}
+                icon={icon}
+                {...rest}
+                onKeyUp={ (e) => {
+                    if (e.keyCode !== 13) {
+                        value = e.target.value;
+                    } else {
+                        if (value && value !== "") onSearch(value);
+                    }
+                }}
+            />
+        </div>
+    );
+};
+
+export default SearchInput;
