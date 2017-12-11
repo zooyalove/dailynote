325547848cfb790dcab913325321370a88464fb2
ZooyaLove <dorry457@nate.com>
2017-12-12 03:22:04 +0900

    검색페이지 일부 수정정

--- daily-frontend/src/containers/routes/SearchRoute.js
+++ daily-frontend/src/containers/routes/SearchRoute.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+import SearchInput from 'components/SearchInput';
+
+import * as api from 'helpers/WebApi/note';
 
 class SearchRoute extends Component {
+	state = {
+		fetch: false
+	}
 
 	componentWillMount() {
 		document.title = 'Daily Note - 검색';
 	}
 
+	handleSearch = async (value) => {
+		this.setState({ fetch: true });
+
+		const result = await api.searchNotes(value);
+		
+		window.setTimeout(() => this.setState({ fetch: false }), 3000);
+	}
+
 	render() {
+		const { handleSearch, state: { fetch } } = this;
 		return (
-			<div>
-				SearchRoute 페이지입니다.
+			<div className="subcontents-wrapper">
+				<span style={{marginRight: '1rem', fontWeight: 'bold', fontSize: '1.4rem'}}>찾고자 하는 검색어 입력 :</span>
+				<SearchInput placeholder="찾고 싶은 장부의 내용들을 검색해보세요!" icon onSearch={handleSearch}/>
+				{ fetch && <Dimmer active><Loader>Data Loading...</Loader></Dimmer> }
 			</div>
 		);
 	}
