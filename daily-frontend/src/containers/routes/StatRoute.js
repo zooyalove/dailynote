2caf778bf636f93a75a3451468c2cd7655e7c21b
ZooyaLove <dorry457@nate.com>
2017-12-18 04:17:42 +0900

    통계페이지 수정 시작

--- daily-frontend/src/containers/routes/StatRoute.js
+++ daily-frontend/src/containers/routes/StatRoute.js
@@ -1,4 +1,34 @@
 import React, { Component } from 'react';
+import { Dropdown } from 'semantic-ui-react';
+
+import Card from 'components/Card';
+
+const productCategory = [
+	'꽃다발',
+	'꽃바구니',
+	'꽃상자',
+	'동양란',
+	'서양란',
+	'관엽식물',
+	'영정바구니',
+	'근조화환',
+	'축하화환',
+	'과일바구니',
+	'기타'
+];
+
+const ProductTitle = () => {
+	return (
+		<div>
+			▶ 상품종류별 현황
+			<Dropdown
+				selection
+				placeholder="상품종류"
+				options={productCategory.map((c, i) => { return {'key': i, 'text': c, 'value': c}; })}
+			/>
+		</div>
+	);
+};
 
 class StatRoute extends Component {
 
@@ -8,8 +38,13 @@ class StatRoute extends Component {
 
 	render() {
 		return (
-			<div>
-				StatRoute 페이지입니다.
+			<div className="subcontents-wrapper">
+				<Card
+					className="category-graph"
+					title={<ProductTitle />}
+				>
+					StatRoute 페이지입니다.
+				</Card>
 			</div>
 		);
 	}
