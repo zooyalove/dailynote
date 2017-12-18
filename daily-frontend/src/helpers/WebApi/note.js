c312ec9a2b7b27d120b5c5c0076993b5fc3395a7
Park Kyungbong <dorry457@nate.com>
2017-12-18 19:56:15 +0900

    상품종류에 대한 통계 API 설정

--- daily-frontend/src/helpers/WebApi/note.js
+++ daily-frontend/src/helpers/WebApi/note.js
@@ -12,19 +12,28 @@ export const getAllNotes = () => {
 
 // 특정 장부 가져오기
 export const getNote = (id, params={}) => {
-    return axios.get(`/api/note/${id}`, params);
+    return axios.get(`/api/note/${id}`, { params });
 };
 
 // 오늘의 장부자료 가져오기
 export const getTodayNotes = () => {
     return axios.get('/api/note/today');
-}
+};
 
 // 특정 검색어로 장부 검색하기
 export const searchNotes = (searchTxt) => {
     return axios.get(`/api/note/search/${searchTxt}`);
-}
+};
 
 export const getMonthNotes = (month) => {
     return axios.get(`/api/note/month/${month}`);
-}
+};
+
+export const getCategoryStatistics = (categoryValue, {...filters}) => {
+    return axios.get('/api/note/stat', {
+        params: {
+            category: categoryValue,
+            ...filters
+        }
+    });
+};
