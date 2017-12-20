439a0c99db02b8eb4c8a5e0298bd72ebcdda7c0f
Park Kyungbong <dorry457@nate.com>
2017-12-20 17:47:53 +0900

    카테고리통계부분 필터링 추가

--- daily-frontend/src/components/CategoryStatistics/CategoryStatistics.js
+++ daily-frontend/src/components/CategoryStatistics/CategoryStatistics.js
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import styles from './CategoryStatistics.scss';
 import { Dropdown } from 'semantic-ui-react';
 
+import Input from 'components/Input';
+
 const cx = classNames.bind(styles);
 
 const productCategory = [
@@ -24,15 +26,25 @@ const CategoryStatistics = ({
     onChange
 }) => {
     return (
-		<div>
-			▶ 상품종류별 현황
-			<Dropdown
-				selection
-				placeholder="상품종류"
-				options={productCategory.map((c, i) => { return {'key': i, 'text': c, 'value': c}; })}
-				value={value}
-				onChange={onChange}
-			/>
+		<div className={cx('category-stat-wrapper')}>
+			<div className={cx('category-header')}>
+				▶ 상품종류별 현황
+				<Dropdown
+					selection
+					placeholder="상품종류"
+					options={productCategory.map((c, i) => { return {'key': i, 'text': c, 'value': c}; })}
+					value={value}
+					onChange={onChange}
+				/>
+			</div>
+			<div className={cx('category-filter')}>
+				필터링
+				<Input
+					type="text"
+					onChange={(e) => { console.log(e.target.value); }}
+					onKeyUp={(e) => { console.log(e); }}
+				/>
+			</div>
 		</div>
     );
 };
