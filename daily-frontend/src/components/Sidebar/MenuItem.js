3f0619c3cdea36d7e0b45a3ec5e208a2060181c3
Park Kyungbong <dorry457@nate.com>
2018-01-05 13:45:55 +0900

    모바일버젼에서 클릭시 조그마한 사각형 안보이게하기

--- daily-frontend/src/components/Sidebar/MenuItem.js
+++ daily-frontend/src/components/Sidebar/MenuItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Popup, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router';
 
-const MenuItem = ({ children, color, content, icon, to }) => {
+const MenuItem = ({ children, color, content, icon, to, onClick }) => {
     return (
     	<Popup
         	trigger={
-	          	<Link to={to} activeClassName="active" className={`menuitem ${color}`} >
+	          	<Link to={to} activeClassName="active" className={`menuitem ${color}`} onClick={onClick} >
 	            	<Icon name={icon} />
 	        	</Link>
 	        }
