751360b28263975a6c59a3b9194f640ff05d4679
ZooyaLove <dorry457@nate.com>
2018-01-31 04:05:35 +0900

    pager modified start

--- daily-frontend/src/components/DataTable/DataTable.js
+++ daily-frontend/src/components/DataTable/DataTable.js
@@ -8,11 +8,33 @@ import Pager from 'components/Pager';
 const cx = classNames.bind(styles);
 
 class DataTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentPage: 1,
+            datas: ((props.datas.length > 0) ? props.datas : [])
+        };
+    }
+
+    handlePageClick = (page) => {
+        this.setState({ currentPage: page });
+    }
+
     render() {
+        const { handlePageClick, state: { currentPage, datas } } = this;
+        const { countPerPage, displayPage } = this.props;
+
         return (
             <div className={cx('data-table')}>
                 <DataList/>
-                <Pager />
+                <Pager
+                    datas={datas}
+                    current={currentPage}
+                    countPerPage={countPerPage}
+                    displayPage={displayPage}
+                    onPageClick={handlePageClick}
+                />
             </div>
         );
     }
--- daily-frontend/src/components/Pager/Pager.js
+++ daily-frontend/src/components/Pager/Pager.js
@@ -3,38 +3,22 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './Pager.scss';
 
-import DataList from 'components/DataList';
-
 const cx = classNames.bind(styles);
 
-class Pager extends Component {
-    static defaultProps = {
-        countPerPage: 10
-    }
-    
-    state = {
-        datas: null
-    }
+const Pager = ({
+    current,
+    countPerPage=10,
+    displayPage=5,
+    datas=[],
+    onPageClick
+}) => {
 
-    componentWillMount() {
-        
-    }
+    const dataLength = (datas.length > 0) ? datas.length : 0;
 
-    render() {
-        return (
-            <ul className={cx('pager-wrapper')}>
-            </ul>
-        );
-    }
-};
-
-Pager.propTypes = {
-    datas: PropTypes.array.isRequired,
-    countPerPage: PropTypes.number,
-    ordererView: PropTypes.bool,
-    animation: PropTypes.bool,
-    hide: PropTypes.bool,
-    className: PropTypes.string
+    return (
+        <ul className={cx('pager-wrapper')}>
+        </ul>
+    );
 };
 
 export default Pager;
