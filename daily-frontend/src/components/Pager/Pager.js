9740f565f044aec8f0b29eb47fe566d3ad9ae15e
Park Kyungbong <dorry457@nate.com>
2018-02-01 19:57:15 +0900

    페이지 리스트 작성

--- daily-frontend/src/components/Pager/Pager.js
+++ daily-frontend/src/components/Pager/Pager.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
 import classNames from 'classnames';
 import styles from './Pager.scss';
 
@@ -8,15 +8,33 @@ const cx = classNames.bind(styles);
 const PageNumber = ({
     page,
     className,
+    children,
     onClick
 }) => {
     return (
-        <li className={className} onClick={() => onClick(page)}>{page}</li>
+        <li
+            className={`${className ? className : 'pager-number'}`}
+            onClick={() => onClick(page)}
+        >
+            {children ? children : page}
+        </li>
     );
 }
 
 const FirstPage = () => {
-    return <PageNumber page={1} className="pager-start" />
+    return <PageNumber page={1} className="pager-first"><Icon name="fast backward" /></PageNumber>
+}
+
+const LastPage = ({page}) => {
+    return <PageNumber page={page} className="pager-last"><Icon name="fast forward" /></PageNumber>
+}
+
+const PrevPage = ({page}) => {
+    return <PageNumber page={page} className="pager-prev"><Icon name="" /></PageNumber>
+}
+
+const NextPage = ({page}) => {
+    return <PageNumber page={page} className="pager-next"><Icon name="" /></PageNumber>
 }
 
 const Pager = ({
@@ -28,9 +46,14 @@ const Pager = ({
 }) => {
 
     const dataLength = (datas.length > 0) ? datas.length : 0;
+    const totalPage = Math.ceil(dataLength/countPerPage);
 
     return (
         <ul className={cx('pager-wrapper')}>
+            <FirstPage />
+            <PrevPage page={(current === 1 ? 1 : (current - 1))} />
+            <NextPage page={(current === totalPage ? totalPage : (current + 1))} />
+            <LastPage page={totalPage} />
         </ul>
     );
 };
