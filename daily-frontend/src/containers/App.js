1014b39b4be9c64422f1f5ca57b9ab4c4e359ab1
Park Kyungbong <dorry457@nate.com>
2019-06-20 21:18:28 +0900

    로그아웃시 거래처 정보 초기화

--- daily-frontend/src/containers/App.js
+++ daily-frontend/src/containers/App.js
@@ -1,133 +1,183 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Dimmer, Icon } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Dimmer, Icon } from "semantic-ui-react";
 
-import * as headerAction from 'redux/modules/base/header';
-import * as ordererAction from 'redux/modules/base/orderer';
+import * as headerAction from "redux/modules/base/header";
+import * as ordererAction from "redux/modules/base/orderer";
 
-import Header, { Logo, UserInfo } from 'components/Header';
-import Sidebar, { MenuItem } from 'components/Sidebar';
-import Contents from 'components/Content';
-import Fab from 'components/Fab';
+import Header, { Logo, UserInfo } from "components/Header";
+import Sidebar, { MenuItem } from "components/Sidebar";
+import Contents from "components/Content";
+import Fab from "components/Fab";
 
-import * as user from 'helpers/WebApi/user';
-import storage from 'helpers/storage';
+import * as user from "helpers/WebApi/user";
+import storage from "helpers/storage";
 
 class App extends Component {
+    state = {
+        active: false,
+        scrolling: false
+    };
 
-	state = {
-		active: false,
-		scrolling: false
-	}
+    static contextTypes = {
+        router: React.PropTypes.object
+    };
 
-	static contextTypes = {
-		router: React.PropTypes.object
-	}
+    componentWillMount() {
+        const {
+            HeaderActions,
+            status: { header }
+        } = this.props;
 
-	componentWillMount() {
-		const { HeaderActions, status: { header } } = this.props;
+        user.getInfo()
+            .then(info => {
+                if (!header.get("visible")) {
+                    HeaderActions.openHeader();
+                }
+            })
+            .catch(err => {
+                if (storage.get("loginInfo")) {
+                    storage.remove("loginInfo");
+                }
+                this.context.router.push("/login");
+            });
+    }
 
-		user.getInfo()
-			.then( (info) => {
-				if (!header.get('visible')) {
-					HeaderActions.openHeader();
-				}
-			})
-			.catch( (err) => {
-				if (storage.get('loginInfo')) {
-					storage.remove('loginInfo');
-				}
-				this.context.router.push('/login');
-			});
-	}
+    componentDidMount() {
+        const {
+            status: { header }
+        } = this.props;
 
-	componentDidMount() {
-		const { status: { header } } = this.props;
+        if (header.get("visible")) {
+            window.addEventListener("scroll", this.handleScroll);
+        }
+    }
 
-		if (header.get('visible')) {
-			window.addEventListener('scroll', this.handleScroll);
-		}
-	}
+    componentWillUnmount() {
+        const {
+            OrdererActions,
+            status: { header }
+        } = this.props;
 
-	componentWillUnmount() {
-		const { status: { header } } = this.props;
-		
-		if (header.get('visible')) {
-			window.removeEventListener('scroll', this.handleScroll);
-		}
+        OrdererActions.clearOrdererData();
 
-		if (storage.get('loginInfo')) {
-			storage.remove('loginInfo');
-		}
+        if (header.get("visible")) {
+            window.removeEventListener("scroll", this.handleScroll);
+        }
 
-	}
+        if (storage.get("loginInfo")) {
+            storage.remove("loginInfo");
+        }
+    }
 
-	handleLogOut = async () => {
-		if (storage.get('loginInfo')) {
-			await user.signOut();
+    handleLogOut = async () => {
+        const { OrdererActions } = this.props;
 
-			storage.remove('loginInfo');
-			this.context.router.push('/login');
-		}
-	}
+        if (storage.get("loginInfo")) {
+            await user.signOut();
 
-	handleScroll = (e) => {
-		if (window.scrollY > 220 && !this.state.scrolling) {
-			this.setState({scrolling: true});
-		} else if (window.scrollY <= 210 && this.state.scrolling) {
-			this.setState({scrolling: false});
-		}
-	}
+            storage.remove("loginInfo");
+        }
 
-	handleFabClick = () => {
-		window.scrollTo(0, 0);
-	}
+        OrdererActions.clearOrdererData();
+        this.context.router.push("/login");
+    };
 
-	handleSidebarClick = () => {
-		const { active } = this.state;
+    handleScroll = e => {
+        if (window.scrollY > 220 && !this.state.scrolling) {
+            this.setState({ scrolling: true });
+        } else if (window.scrollY <= 210 && this.state.scrolling) {
+            this.setState({ scrolling: false });
+        }
+    };
 
-		this.setState({
-			active: !active
-		});
-	}
+    handleFabClick = () => {
+        window.scrollTo(0, 0);
+    };
+
+    handleSidebarClick = () => {
+        const { active } = this.state;
+
+        this.setState({
+            active: !active
+        });
+    };
 
     render() {
-    	const { handleLogOut, handleFabClick, handleSidebarClick } = this;
-		const { children, status: { header, orderer } } = this.props;
-		const { active, scrolling } = this.state;
-		const visible = header.get('visible');
-		const username = storage.get('loginInfo') ? storage.get('loginInfo')['username'] : '';
+        const { handleLogOut, handleFabClick, handleSidebarClick } = this;
+        const {
+            children,
+            status: { header, orderer }
+        } = this.props;
+        const { active, scrolling } = this.state;
+        const visible = header.get("visible");
+        const username = storage.get("loginInfo")
+            ? storage.get("loginInfo")["username"]
+            : "";
 
         return (
-			<div>
-			{visible
-			? (
-				<div>
-					<Header>
-						<Logo active={active} onSidebarClick={handleSidebarClick} />
-						<UserInfo username={username} onLogOut={handleLogOut} />
-					</Header>
-					<Sidebar active={active}>
-						<MenuItem color="red" content="장부등록" icon="write" to="/write" onClick={handleSidebarClick} />
-						<MenuItem color="grape" content="거래처 관리" icon="users" to="/orderer" onClick={handleSidebarClick} />
-						<MenuItem color="green" content="장부검색" icon="search" to="/search" onClick={handleSidebarClick} />
-						<MenuItem color="blue" content="통계" icon="bar graph" to="/stat" onClick={handleSidebarClick} />
-					</Sidebar>
-					<Contents>
-						{children}
-						{scrolling && <Fab onAction={handleFabClick}><Icon name="angle double up" size="big"/></Fab> }
-					</Contents>
-	                {orderer.getIn(['modal', 'fetch']) && 
-	                <Dimmer active page>
-	                    {orderer.getIn(['modal', 'message'])}
-	                </Dimmer>
-	                }
-				</div>
-			)
-			: children
-			}
-			</div>
+            <div>
+                {visible ? (
+                    <div>
+                        <Header>
+                            <Logo
+                                active={active}
+                                onSidebarClick={handleSidebarClick}
+                            />
+                            <UserInfo
+                                username={username}
+                                onLogOut={handleLogOut}
+                            />
+                        </Header>
+                        <Sidebar active={active}>
+                            <MenuItem
+                                color="red"
+                                content="장부등록"
+                                icon="write"
+                                to="/write"
+                                onClick={handleSidebarClick}
+                            />
+                            <MenuItem
+                                color="grape"
+                                content="거래처 관리"
+                                icon="users"
+                                to="/orderer"
+                                onClick={handleSidebarClick}
+                            />
+                            <MenuItem
+                                color="green"
+                                content="장부검색"
+                                icon="search"
+                                to="/search"
+                                onClick={handleSidebarClick}
+                            />
+                            <MenuItem
+                                color="blue"
+                                content="통계"
+                                icon="bar graph"
+                                to="/stat"
+                                onClick={handleSidebarClick}
+                            />
+                        </Sidebar>
+                        <Contents>
+                            {children}
+                            {scrolling && (
+                                <Fab onAction={handleFabClick}>
+                                    <Icon name="angle double up" size="big" />
+                                </Fab>
+                            )}
+                        </Contents>
+                        {orderer.getIn(["modal", "fetch"]) && (
+                            <Dimmer active page>
+                                {orderer.getIn(["modal", "message"])}
+                            </Dimmer>
+                        )}
+                    </div>
+                ) : (
+                    children
+                )}
+            </div>
         );
     }
 }
@@ -143,6 +193,6 @@ App = connect(
         HeaderActions: bindActionCreators(headerAction, dispatch),
         OrdererActions: bindActionCreators(ordererAction, dispatch)
     })
- )(App);
+)(App);
 
 export default App;
--- daily-frontend/src/redux/modules/base/orderer.js
+++ daily-frontend/src/redux/modules/base/orderer.js
@@ -1,12 +1,13 @@
-import { createAction, handleActions } from 'redux-actions';
-import { Map, List, fromJS } from 'immutable';
-import _ from 'lodash';
+import { createAction, handleActions } from "redux-actions";
+import { Map, List, fromJS } from "immutable";
+// import _ from 'lodash';
 
 /* actions */
 const ORDERER_ADD_MODAL_OPEN = "orderer/ORDERER_ADD_MODAL_OPEN";
 const ORDERER_MODIFY_INFO_SET = "orderer/ORDERER_MODIFY_INFO_SET";
 const ORDERER_DATA_FETCHING = "orderer/ORDERER_DATA_FETCHING";
 const ORDERER_DATA_SET = "orderer/ORDERER_DATA_SET";
+const ORDERER_DATA_CLEAR = "orderer/ORDERER_DATA_CLEAR";
 const ORDERER_SELECTED_SET = "orderer/ORDERER_SELECTED_SET";
 
 /* action creators */
@@ -14,71 +15,82 @@ export const openAddOrdererModal = createAction(ORDERER_ADD_MODAL_OPEN);
 export const setOrdererModifyInfo = createAction(ORDERER_MODIFY_INFO_SET);
 export const fetchingOrdererData = createAction(ORDERER_DATA_FETCHING);
 export const setOrdererData = createAction(ORDERER_DATA_SET);
+export const clearOrdererData = createAction(ORDERER_DATA_CLEAR);
 export const setSelectedOrderer = createAction(ORDERER_SELECTED_SET);
 
 const initialState = Map({
     modal: Map({
-        mode: '',
+        mode: "",
         open: false,
         fetch: false,
-        message: ''
+        message: ""
     }),
     info: null,
     selected: null,
     data: List([])
 });
 
-export default handleActions({
-	[ORDERER_ADD_MODAL_OPEN]: (state, action) => {
-        const { open, mode } = action.payload;
+export default handleActions(
+    {
+        [ORDERER_ADD_MODAL_OPEN]: (state, action) => {
+            const { open, mode } = action.payload;
 
-        let m = '';
+            let m = "";
 
-        if (typeof mode !== 'undefined') m = mode;
+            if (typeof mode !== "undefined") m = mode;
 
-        return state.setIn(['modal', 'open'], open)
-                    .setIn(['modal', 'mode'], m);
-    },
-    [ORDERER_MODIFY_INFO_SET]: (state, action) => {
-        const { info } = action.payload;
+            return state
+                .setIn(["modal", "open"], open)
+                .setIn(["modal", "mode"], m);
+        },
+        [ORDERER_MODIFY_INFO_SET]: (state, action) => {
+            const { info } = action.payload;
 
-        return state.set('info', info);
-    },
-	[ORDERER_DATA_FETCHING]: (state, action) => {
-        const { fetch, message } = action.payload;
+            return state.set("info", info);
+        },
+        [ORDERER_DATA_FETCHING]: (state, action) => {
+            const { fetch, message } = action.payload;
 
-		return state.setIn(['modal', 'fetch'], fetch)
-                    .setIn(['modal', 'message'], message);
-    },
-	[ORDERER_DATA_SET]: (state, action) => {
-        const { orderer } = action.payload;
+            return state
+                .setIn(["modal", "fetch"], fetch)
+                .setIn(["modal", "message"], message);
+        },
+        [ORDERER_DATA_SET]: (state, action) => {
+            const { orderer } = action.payload;
 
-        let data = state.get('data');
+            let data = state.get("data");
 
-        if (orderer.length === undefined && orderer.size === undefined) {
-            if (!data.isEmpty()) {
-                // console.log("무언가 있을때...");
-                const index = data.findIndex((d) => d.get('_id') === orderer._id);
+            if (orderer.length === undefined && orderer.size === undefined) {
+                if (!data.isEmpty()) {
+                    // console.log("무언가 있을때...");
+                    const index = data.findIndex(
+                        d => d.get("_id") === orderer._id
+                    );
 
-                if (index === -1) {
-                    data = data.insert(0, fromJS(orderer));
-                    return state.set('data', data);
+                    if (index === -1) {
+                        data = data.insert(0, fromJS(orderer));
+                        return state.set("data", data);
+                    } else {
+                        data = data.update(index, v => fromJS(orderer));
+                        return state.set("data", data);
+                    }
                 } else {
-                    data = data.update(index, (v) => fromJS(orderer));
-                    return state.set('data', data);
+                    // console.log("무언가 없을때...");
+                    data = data.push(fromJS(orderer));
+                    return state.set("data", data);
                 }
-            } else {
-                // console.log("무언가 없을때...");
-                data = data.push(fromJS(orderer));
-                return state.set('data', data);
             }
-        }
 
-        return state.set('data', fromJS(orderer));
+            return state.set("data", fromJS(orderer));
+        },
+        [ORDERER_DATA_CLEAR]: (state, action) => {
+            return initialState;
+        },
+        [ORDERER_SELECTED_SET]: (state, action) => {
+            const selectedId = action.payload;
+
+            return state.set("selected", selectedId);
+        }
     },
-    [ORDERER_SELECTED_SET]: (state, action) => {
-        const selectedId = action.payload;
-        
-        return state.set('selected', selectedId);
-    }
-}, initialState);
+    initialState
+);
