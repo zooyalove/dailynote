13f0230e4e8c2ff5e2009dd97a0a96c262989d1a
Zooyalove <dorry457@nate.com>
2019-07-08 02:15:06 +0900

    new Computers installed

--- daily-frontend/src/containers/App.js
+++ daily-frontend/src/containers/App.js
@@ -15,184 +15,179 @@ import * as user from "helpers/WebApi/user";
 import storage from "helpers/storage";
 
 class App extends Component {
-    state = {
-        active: false,
-        scrolling: false
-    };
+  state = {
+    active: false,
+    scrolling: false
+  };
 
-    static contextTypes = {
-        router: React.PropTypes.object
-    };
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
 
-    componentWillMount() {
-        const {
-            HeaderActions,
-            status: { header }
-        } = this.props;
+  componentWillMount() {
+    const {
+      HeaderActions,
+      status: { header }
+    } = this.props;
 
-        user.getInfo()
-            .then(info => {
-                if (!header.get("visible")) {
-                    HeaderActions.openHeader();
-                }
-            })
-            .catch(err => {
-                if (storage.get("loginInfo")) {
-                    storage.remove("loginInfo");
-                }
-                this.context.router.push("/login");
-            });
-    }
-
-    componentDidMount() {
-        const {
-            status: { header }
-        } = this.props;
-
-        if (header.get("visible")) {
-            window.addEventListener("scroll", this.handleScroll);
+    user
+      .getInfo()
+      .then(info => {
+        if (!header.get("visible")) {
+          HeaderActions.openHeader();
         }
-    }
-
-    componentWillUnmount() {
-        const {
-            OrdererActions,
-            status: { header }
-        } = this.props;
-
-        OrdererActions.clearOrdererData();
-
-        if (header.get("visible")) {
-            window.removeEventListener("scroll", this.handleScroll);
-        }
-
+      })
+      .catch(err => {
         if (storage.get("loginInfo")) {
-            storage.remove("loginInfo");
+          storage.remove("loginInfo");
         }
-    }
-
-    handleLogOut = async () => {
-        const { OrdererActions } = this.props;
-
-        if (storage.get("loginInfo")) {
-            await user.signOut();
-
-            storage.remove("loginInfo");
-        }
-
-        OrdererActions.clearOrdererData();
         this.context.router.push("/login");
-    };
+      });
+  }
 
-    handleScroll = e => {
-        if (window.scrollY > 220 && !this.state.scrolling) {
-            this.setState({ scrolling: true });
-        } else if (window.scrollY <= 210 && this.state.scrolling) {
-            this.setState({ scrolling: false });
-        }
-    };
+  componentDidMount() {
+    const {
+      status: { header }
+    } = this.props;
 
-    handleFabClick = () => {
-        window.scrollTo(0, 0);
-    };
-
-    handleSidebarClick = () => {
-        const { active } = this.state;
-
-        this.setState({
-            active: !active
-        });
-    };
-
-    render() {
-        const { handleLogOut, handleFabClick, handleSidebarClick } = this;
-        const {
-            children,
-            status: { header, orderer }
-        } = this.props;
-        const { active, scrolling } = this.state;
-        const visible = header.get("visible");
-        const username = storage.get("loginInfo")
-            ? storage.get("loginInfo")["username"]
-            : "";
-
-        return (
-            <div>
-                {visible ? (
-                    <div>
-                        <Header>
-                            <Logo
-                                active={active}
-                                onSidebarClick={handleSidebarClick}
-                            />
-                            <UserInfo
-                                username={username}
-                                onLogOut={handleLogOut}
-                            />
-                        </Header>
-                        <Sidebar active={active}>
-                            <MenuItem
-                                color="red"
-                                content="장부등록"
-                                icon="write"
-                                to="/write"
-                                onClick={handleSidebarClick}
-                            />
-                            <MenuItem
-                                color="grape"
-                                content="거래처 관리"
-                                icon="users"
-                                to="/orderer"
-                                onClick={handleSidebarClick}
-                            />
-                            <MenuItem
-                                color="green"
-                                content="장부검색"
-                                icon="search"
-                                to="/search"
-                                onClick={handleSidebarClick}
-                            />
-                            <MenuItem
-                                color="blue"
-                                content="통계"
-                                icon="bar graph"
-                                to="/stat"
-                                onClick={handleSidebarClick}
-                            />
-                        </Sidebar>
-                        <Contents>
-                            {children}
-                            {scrolling && (
-                                <Fab onAction={handleFabClick}>
-                                    <Icon name="angle double up" size="big" />
-                                </Fab>
-                            )}
-                        </Contents>
-                        {orderer.getIn(["modal", "fetch"]) && (
-                            <Dimmer active page>
-                                {orderer.getIn(["modal", "message"])}
-                            </Dimmer>
-                        )}
-                    </div>
-                ) : (
-                    children
-                )}
-            </div>
-        );
+    if (header.get("visible")) {
+      window.addEventListener("scroll", this.handleScroll);
     }
+  }
+
+  componentWillUnmount() {
+    const {
+      OrdererActions,
+      status: { header }
+    } = this.props;
+
+    OrdererActions.clearOrdererData();
+
+    if (header.get("visible")) {
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+
+    if (storage.get("loginInfo")) {
+      storage.remove("loginInfo");
+    }
+  }
+
+  handleLogOut = async () => {
+    const { OrdererActions } = this.props;
+
+    if (storage.get("loginInfo")) {
+      await user.signOut();
+
+      storage.remove("loginInfo");
+    }
+
+    OrdererActions.clearOrdererData();
+    this.context.router.push("/login");
+  };
+
+  handleScroll = e => {
+    if (window.scrollY > 220 && !this.state.scrolling) {
+      this.setState({ scrolling: true });
+    } else if (window.scrollY <= 210 && this.state.scrolling) {
+      this.setState({ scrolling: false });
+    }
+  };
+
+  handleFabClick = () => {
+    window.scrollTo(0, 0);
+  };
+
+  handleSidebarClick = () => {
+    const { active } = this.state;
+
+    this.setState({
+      active: !active
+    });
+  };
+
+  render() {
+    const { handleLogOut, handleFabClick, handleSidebarClick } = this;
+    const {
+      children,
+      status: { header, orderer }
+    } = this.props;
+    const { active, scrolling } = this.state;
+    const visible = header.get("visible");
+    const username = storage.get("loginInfo")
+      ? storage.get("loginInfo")["username"]
+      : "";
+
+    return (
+      <div>
+        {visible ? (
+          <div>
+            <Header>
+              <Logo active={active} onSidebarClick={handleSidebarClick} />
+              <UserInfo username={username} onLogOut={handleLogOut} />
+            </Header>
+            <Sidebar active={active}>
+              <MenuItem
+                color="red"
+                content="장부등록"
+                icon="write"
+                to="/write"
+                onClick={handleSidebarClick}
+              />
+              <MenuItem
+                color="grape"
+                content="거래처 관리"
+                icon="users"
+                to="/orderer"
+                onClick={handleSidebarClick}
+              />
+              <MenuItem
+                color="green"
+                content="장부검색"
+                icon="search"
+                to="/search"
+                onClick={handleSidebarClick}
+              />
+              <MenuItem
+                color="blue"
+                content="통계"
+                icon="bar graph"
+                to="/stat"
+                onClick={handleSidebarClick}
+              />
+            </Sidebar>
+            <Contents>
+              {children}
+              {scrolling && (
+                <Fab onAction={handleFabClick}>
+                  <Icon name="angle double up" size="big" />
+                </Fab>
+              )}
+            </Contents>
+            {orderer.getIn(["modal", "fetch"]) && (
+              <Dimmer active page>
+                {orderer.getIn(["modal", "message"])}
+              </Dimmer>
+            )}
+          </div>
+        ) : (
+          children
+        )}
+      </div>
+    );
+  }
 }
 
 App = connect(
-    state => ({
-        status: {
-            header: state.base.header,
-            orderer: state.base.orderer
-        }
-    }),
-    dispatch => ({
-        HeaderActions: bindActionCreators(headerAction, dispatch),
-        OrdererActions: bindActionCreators(ordererAction, dispatch)
-    })
+  state => ({
+    status: {
+      header: state.base.header,
+      orderer: state.base.orderer
+    }
+  }),
+  dispatch => ({
+    HeaderActions: bindActionCreators(headerAction, dispatch),
+    OrdererActions: bindActionCreators(ordererAction, dispatch)
+  })
 )(App);
 
 export default App;
