76212c45be8782034740ef70e70fe9b3b61829c1
Park Kyungbong <dorry457@nate.com>
2019-07-12 21:30:45 +0900

    DayPicker의 Custom Caption Component 추가

--- daily-frontend/src/containers/routes/SearchRoute.js
+++ daily-frontend/src/containers/routes/SearchRoute.js
@@ -1,136 +1,221 @@
-import React, { Component } from 'react';
-import { Dimmer, Loader, Button } from 'semantic-ui-react';
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
+import React, { Component } from "react";
+import { Dimmer, Loader, Button } from "semantic-ui-react";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
-import MomentLocaleUtils from 'react-day-picker/moment';
-import 'moment/locale/ko';
+import MomentLocaleUtils from "react-day-picker/moment";
+import "moment/locale/ko";
 
-import SearchInput from 'components/SearchInput';
-import DataTable from 'components/DataTable';
-import Card from 'components/Card';
+import SearchInput from "components/SearchInput";
+import DataTable from "components/DataTable";
+import Card from "components/Card";
+import DayPickerCaption from "components/DayPickerCaption";
 
-import * as api from 'helpers/WebApi/note';
-import * as utils from 'helpers/utils';
+import * as api from "helpers/WebApi/note";
+import * as utils from "helpers/utils";
 
 class SearchRoute extends Component {
-	state = {
-		fetch: false,
-		datas: [],
-		monthData: [],
-		searchTxt: null,
-		selectedDays: null,
-		selectedMonth: null,
-		calendarActive: false
-	}
+    state = {
+        fetch: false,
+        datas: [],
+        monthData: [],
+        searchTxt: null,
+        selectedDays: null,
+        selectedMonth: null,
+        calendarActive: false,
+        month: new Date()
+    };
 
-	componentWillMount() {
-		document.title = 'Daily Note - 검색';
+    componentWillMount() {
+        document.title = "Daily Note - 검색";
 
-		this.getMonthNotes(new Date());
-	}
+        this.getMonthNotes(new Date());
+    }
 
-	getMonthNotes = async (month) => {
-		this.setState({ selectedMonth: `${month.getFullYear()}-${month.getMonth()}`});
+    getMonthNotes = async month => {
+        this.setState({
+            selectedMonth: `${month.getFullYear()}-${month.getMonth()}`
+        });
 
-		const date = month.getFullYear() + '-' + (month.getMonth()+1);
-		const res = await api.getMonthNotes(date);
-		const data = res.data.data;
-		console.log(data);
-		const monthData = [];
+        const date = month.getFullYear() + "-" + (month.getMonth() + 1);
+        const res = await api.getMonthNotes(date);
+        const data = res.data.data;
+        // console.log(data);
+        const monthData = [];
 
-		if (data.length > 0) {
-			data.forEach( (d) => {
-				monthData.push(d._id);
-			});
-		}
+        if (data.length > 0) {
+            data.forEach(d => {
+                monthData.push(d._id);
+            });
+        }
 
-		this.setState({ monthData });
-	}
+        this.setState({ monthData });
+    };
 
-	existOrder = (day) => {
-		const { state: { monthData, selectedMonth }} = this;
-		const yearMonth = `${day.getFullYear()}-${day.getMonth()}`;
+    existOrder = day => {
+        const {
+            state: { monthData, selectedMonth }
+        } = this;
+        const yearMonth = `${day.getFullYear()}-${day.getMonth()}`;
 
-		return monthData.includes(day.getDate()) && selectedMonth === yearMonth;
-	}
+        return monthData.includes(day.getDate()) && selectedMonth === yearMonth;
+    };
 
-	handleSearch = async (value) => {
-		this.setState({ fetch: true, datas: [], searchTxt: value, selectedDays: null });
+    handleSearch = async value => {
+        this.setState({
+            fetch: true,
+            datas: [],
+            searchTxt: value,
+            selectedDays: null
+        });
 
-		const result = await api.searchNotes(value);
-		const data = result.data.data;
+        const result = await api.searchNotes(value);
+        const data = result.data.data;
 
-		window.setTimeout(() => {
-			this.setState({ fetch: false, datas: (utils.empty(data) ? [] : data) });
-		}, 2000);
-	}
+        window.setTimeout(() => {
+            this.setState({
+                fetch: false,
+                datas: utils.empty(data) ? [] : data
+            });
+        }, 2000);
+    };
 
-	handlePickerSearch = async (day, { selected }) => {
-		if (!selected) {
-			let date = day.toLocaleDateString().replace(/ /g, '');
-			if (date.indexOf('/') > -1) {
-				date = date.split('/');
-				date = [].concat([date[2], date[0], date[1]]);
-				date = date.join('-');
-			} else {
-				date = date.replace(/\./g, '-').substring(0, date.length - 1);
-			}
+    handlePickerSearch = async (day, { selected }) => {
+        if (!selected) {
+            let date = day.toLocaleDateString().replace(/ /g, "");
+            if (date.indexOf("/") > -1) {
+                date = date.split("/");
+                date = [].concat([date[2], date[0], date[1]]);
+                date = date.join("-");
+            } else {
+                date = date.replace(/\./g, "-").substring(0, date.length - 1);
+            }
 
-			this.setState({ fetch: true, datas: [], searchTxt: date, selectedDays: day });
+            this.setState({
+                fetch: true,
+                datas: [],
+                searchTxt: date,
+                selectedDays: day
+            });
 
-			const result = await api.searchNotes(date);
-			const data = result.data.data;
+            const result = await api.searchNotes(date);
+            const data = result.data.data;
 
-			window.setTimeout(() => {
-				this.setState({ fetch: false, datas: (utils.empty(data) ? [] : data),  calendarActive: false});
-			}, 2000);
-		}
-	}
+            window.setTimeout(() => {
+                this.setState({
+                    fetch: false,
+                    datas: utils.empty(data) ? [] : data,
+                    calendarActive: false
+                });
+            }, 2000);
+        }
+    };
 
-	render() {
-		const { existOrder, getMonthNotes, handleSearch, handlePickerSearch, state: { datas, fetch, searchTxt, selectedDays, calendarActive } } = this;
-		
-		let total = 0;
-		
-		if (datas) {
-			datas.forEach( (d) => {
-				total += d.delivery.price * d.delivery.count;
-			});
-		}
+    handleCaptionClick = month => {
+        this.setState({ month });
+    };
 
-		return (
-			<div className="subcontents-wrapper flex">
-				<div className="search-list-wrapper">
-					<span style={{marginRight: '1rem', fontWeight: 'bold', fontSize: '1.4rem'}}>찾고자 하는 검색어 입력 :</span>
-					<SearchInput placeholder="찾고 싶은 장부의 내용들을 검색해보세요!" icon onSearch={handleSearch}/>
-					{ fetch && <Dimmer active><Loader>Data Loading...</Loader></Dimmer> }
-					{ searchTxt && <div className="search-txt"><b>▶</b> 입력하신 검색어는 <span>{searchTxt}</span></div> }
-					<div className="total-price-wrapper">총합계 <span className="price">{total === 0 ? total : total.toLocaleString()}</span>원</div>
-					<DataTable
-						datas={datas}
-						countPerPage={10}
-						displayPage={5}
-						ordererView
-						style={{marginTop: '2rem'}} />
-				</div>
-				<div className="day-picker">
-					<Button circular icon="calendar" className={`calendar-icon${calendarActive ? ' active' : ''}`} onClick={() => this.setState({ calendarActive: !calendarActive })} />
-					<Card className={`${calendarActive ? 'active' : ''}`}>
-						<DayPicker
-							localeUtils={MomentLocaleUtils}
-							locale="ko"
-							modifiers={{ existOrder }}
-							selectedDays={selectedDays}
-							todayButton="Go to Today"
-							onMonthChange={getMonthNotes}
-							onDayClick={handlePickerSearch}
-						/>
-					</Card>
-				</div>
-			</div>
-		);
-	}
-};
+    render() {
+        const {
+            existOrder,
+            getMonthNotes,
+            handleSearch,
+            handlePickerSearch,
+            handleCaptionClick,
+            state: {
+                datas,
+                fetch,
+                searchTxt,
+                selectedDays,
+                calendarActive,
+                month
+            }
+        } = this;
+
+        let total = 0;
+
+        if (datas) {
+            datas.forEach(d => {
+                total += d.delivery.price * d.delivery.count;
+            });
+        }
+
+        return (
+            <div className="subcontents-wrapper flex">
+                <div className="search-list-wrapper">
+                    <span
+                        style={{
+                            marginRight: "1rem",
+                            fontWeight: "bold",
+                            fontSize: "1.4rem"
+                        }}
+                    >
+                        찾고자 하는 검색어 입력 :
+                    </span>
+                    <SearchInput
+                        placeholder="찾고 싶은 장부의 내용들을 검색해보세요!"
+                        icon
+                        onSearch={handleSearch}
+                    />
+                    {fetch && (
+                        <Dimmer active>
+                            <Loader>Data Loading...</Loader>
+                        </Dimmer>
+                    )}
+                    {searchTxt && (
+                        <div className="search-txt">
+                            <b>▶</b> 입력하신 검색어는 <span>{searchTxt}</span>
+                        </div>
+                    )}
+                    <div className="total-price-wrapper">
+                        총합계{" "}
+                        <span className="price">
+                            {total === 0 ? total : total.toLocaleString()}
+                        </span>
+                        원
+                    </div>
+                    <DataTable
+                        datas={datas}
+                        countPerPage={10}
+                        displayPage={5}
+                        ordererView
+                        style={{ marginTop: "2rem" }}
+                    />
+                </div>
+                <div className="day-picker">
+                    <Button
+                        circular
+                        icon="calendar"
+                        className={`calendar-icon${
+                            calendarActive ? " active" : ""
+                        }`}
+                        onClick={() =>
+                            this.setState({ calendarActive: !calendarActive })
+                        }
+                    />
+                    <Card className={`${calendarActive ? "active" : ""}`}>
+                        <DayPicker
+                            localeUtils={MomentLocaleUtils}
+                            locale="ko"
+                            month={month}
+                            modifiers={{ existOrder }}
+                            selectedDays={selectedDays}
+                            todayButton="Go to Today"
+                            captionElement={({ date, localeUtils }) => (
+                                <DayPickerCaption
+                                    date={date}
+                                    localeUtils={localeUtils}
+                                    onCaptionClick={handleCaptionClick}
+                                />
+                            )}
+                            onMonthChange={getMonthNotes}
+                            onDayClick={handlePickerSearch}
+                        />
+                    </Card>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default SearchRoute;
