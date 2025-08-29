adc5ede29224d39a4177c5c3bad924987846e383
Zylovue <dorry457@gmail.com>
2025-08-30 01:11:07 +0900

    ListRow 컴포넌트에 필요없는 데이터는 패싱하기

--- daily-frontend/src/components/ListRow/ListRow.js
+++ daily-frontend/src/components/ListRow/ListRow.js
@@ -1,72 +1,84 @@
-import React from 'react';
-import moment from 'moment';
-import classNames from 'classnames';
-import styles from './ListRow.scss';
+import React from "react";
+import moment from "moment";
+import classNames from "classnames";
+import styles from "./ListRow.scss";
 
-import ListColumn from 'components/ListColumn';
+import ListColumn from "components/ListColumn";
 
-import { empty } from 'helpers/utils';
+import { empty } from "helpers/utils";
 
 const cx = classNames.bind(styles);
 
-const ListRow = ({
-    index,
-    data,
-    ordererView,
-    children
-}) => {
-    if (!data) {
-        return (
-            <div className={cx('list-row')}>
-                <ListColumn text="해당되는 내용이 없습니다!" center red />
-            </div>
-        );
-    }
-
-    const { delivery, receiver, orderer, memo } = data;
-
-    let oData = null;
-    if (ordererView) {
-        oData = Object.keys(orderer).map((o, i) => {
-            if (o === '_id' || o === 'id') return null;
-            return <ListColumn key={i} text={orderer[o]} title={((o === 'name' && !empty(memo)) ? memo : '')} center />;
-        });
-    }
-
-    const deliveries = [];
-    
-    Object.keys(delivery).forEach((d, i) => {
-        if (d === 'date') {
-            // console.log(i);
-            let dates = moment(new Date(delivery[d])).format('YYYY-MM-DD A hh시 mm분');
-            dates = dates.replace('AM', '오전').replace('PM', '오후');
-            deliveries.push(<ListColumn key={i} text={dates} center date />);
-        } else if (d === 'address') {
-            deliveries.push(<ListColumn key={i} text={delivery[d]} address />);
-        } else if (d === 'price') {
-            deliveries.push(<ListColumn key={i} text={(delivery[d] * delivery['count']).toLocaleString()} right />);
-        } else if (d === 'category') {
-            deliveries.push(<ListColumn key={i} text={delivery[d]} center className="category" />);
-        } else {
-            if (d !== 'count') {
-                // console.log("Else", i);
-                deliveries.push(<ListColumn key={i} text={delivery[d]} center />);
-            }
-        }
-    });
-
-    const recv = Object.keys(receiver).map((r, i) => {
-        return <ListColumn key={i} text={receiver[r]} center />;
-    });
-
+const ListRow = ({ index, data, ordererView, children }) => {
+  if (!data) {
     return (
-        <div className={cx('list-row')}>
-            <ListColumn text={index} center className="no" />
-            {ordererView && oData}
-            {recv}
-            {deliveries}
-        </div>
+      <div className={cx("list-row")}>
+        <ListColumn text="해당되는 내용이 없습니다!" center red />
+      </div>
     );
+  }
+
+  const { delivery, receiver, orderer, memo } = data;
+
+  let oData = null;
+  if (ordererView) {
+    oData = Object.keys(orderer).map((o, i) => {
+      if (o === "_id" || o === "id") return null;
+      return (
+        <ListColumn
+          key={i}
+          text={orderer[o]}
+          title={o === "name" && !empty(memo) ? memo : ""}
+          center
+        />
+      );
+    });
+  }
+
+  const deliveries = [];
+
+  Object.keys(delivery).forEach((d, i) => {
+    if (d === "date") {
+      // console.log(i);
+      let dates = moment(new Date(delivery[d])).format(
+        "YYYY-MM-DD A hh시 mm분"
+      );
+      dates = dates.replace("AM", "오전").replace("PM", "오후");
+      deliveries.push(<ListColumn key={i} text={dates} center date />);
+    } else if (d === "address") {
+      deliveries.push(<ListColumn key={i} text={delivery[d]} address />);
+    } else if (d === "price") {
+      deliveries.push(
+        <ListColumn
+          key={i}
+          text={(delivery[d] * delivery["count"]).toLocaleString()}
+          right
+        />
+      );
+    } else if (d === "category") {
+      deliveries.push(
+        <ListColumn key={i} text={delivery[d]} center className="category" />
+      );
+    } else {
+      if (d !== "count" && d !== "discount") {
+        // console.log("Else", i);
+        deliveries.push(<ListColumn key={i} text={delivery[d]} center />);
+      }
+    }
+  });
+
+  const recv = Object.keys(receiver).map((r, i) => {
+    return <ListColumn key={i} text={receiver[r]} center />;
+  });
+
+  return (
+    <div className={cx("list-row")}>
+      <ListColumn text={index} center className="no" />
+      {ordererView && oData}
+      {recv}
+      {deliveries}
+    </div>
+  );
 };
 
 export default ListRow;
